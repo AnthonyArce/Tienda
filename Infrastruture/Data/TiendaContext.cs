3f713bf30295f2982023743b78c8db57971a85ba
AnthonyArce <anthony_cai_1997@outlook.com>
2025-03-11 23:18:09 -0300

    Agregar archivos de proyecto.

---
+++ Infrastruture/Data/TiendaContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastruture.Data
+{
+    public class TiendaContext:DbContext
+    {
+        public TiendaContext(DbContextOptions<TiendaContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Producto> Producto { get; set; }
+    }
+}
