97eeeb092f0ed3e89473ad6e9fd9cfdfcc45b5d1
Anthony Obregon <anthony_cai_1997@outlook.com>
2025-03-22 11:06:27 -0300

    first commit

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,16 @@
+using Infrastruture.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TiendaContext>(option => {
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +23,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+ using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+        try
+        {
+            var context = services.GetRequiredService<TiendaContext>();
+            await context.Database.MigrateAsync();
+        }
+        catch (Exception ex)
+        {
+            var logger = loggerFactory.CreateLogger<Program>();
+            logger.LogError(ex, "Ocurrio un error durante la migracion");
+        }
+    }
 
 app.UseHttpsRedirection();
 
--- Infrastruture/Data/TiendaContext.cs
+++ Infrastruture/Data/TiendaContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +17,14 @@ namespace Infrastruture.Data
         }
 
         public DbSet<Producto> Producto { get; set; }
+        public DbSet<Categoria> categoria { get; set; }
+        public DbSet<Marca> Marca { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
